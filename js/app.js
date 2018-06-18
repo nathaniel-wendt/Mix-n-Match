70d9081257e3fa2098bab3baee05a3d030ef09e0
Nathaniel <nathaniel.wendt@gmail.com>
2018-06-17 19:25:51 -0600

    feat: add moves counter

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+
+let selectedCards = [];
+let moveCount = 0;
+
 const cards = [
     "fa-diamond", "fa-diamond",
     "fa-bomb", "fa-bomb",
@@ -9,8 +13,12 @@ const cards = [
     "fa-bolt", "fa-bolt",
   ];
 
-function generateCards(card) {
-    return `<li class="card"><i class="fa ${card}"></i> </li>`;
+// moves counter
+const moves = document.querySelector('.moves');
+
+function addMove() {
+    moveCount++;
+    moves.textContent = moveCount;
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -28,29 +36,29 @@ function shuffle(array) {
     return array;
 }
 
+// Programatically generates card HTML
+function generateCards(card) {
+    return `<li class="card"><i class="fa ${card}"></i> </li>`;
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// 
+function newGame() {
+    moveCount = 0;
+    moves.textContent = 0;
+    shuffleDeck();
+}
 
-function startGame() {
+newGame();
+
+function shuffleDeck() {
     const deck = document.querySelector('.deck');
     let cardHTML = shuffle(cards).map(function(card) {
         return generateCards(card);
     });
+
     deck.innerHTML = cardHTML.join('');
-}
+};
 
-startGame();
-
-let selectedCards = [];
 
 // flip card on click
 
@@ -64,6 +72,7 @@ document.querySelector('.deck').addEventListener('click', event => {
             addCard(currentCard);
         if (selectedCards.length === 2) {
             checkForMatch();
+            addMove();
         }
     }
 });
